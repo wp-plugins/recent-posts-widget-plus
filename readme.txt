6c5aa201a7d006b3fa83b38f1b7cbf035ec62b15
vanderwijk <vanderwijk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-26 16:08:55 +0000

    Initial release
    git-svn-id: https://plugins.svn.wordpress.org/recent-posts-widget-plus/trunk@564116 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,49 @@
+=== Recent Posts Widget Plus ===
+Contributors: vanderwijk
+Author URI: http://www.vanderwijk.com/
+Donate link: http://www.vanderwijk.com/wordpress/support/
+Tags: widget, sidebar, recent post, recent posts, excerpt, recent, post
+Requires at least: 2.9.2
+Tested up to: 3.4
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin adds a widget to WordPress that allows you to display the recent posts with an exerpt in the sidebar.
+
+== Description ==
+
+The Recent Posts Widget Plus plugin allows you to display a list of the most recent posts from all or a specific category or tag. It not only shows the post title, but also an excerpt of the post.
+
+== Screenshots ==
+
+1. You can set the widget title, the number of posts, the length of the excerpt and select a category to display the recent posts from or choose to display the recent posts with a certain tag.
+
+== Installation ==
+
+1. First you will have to upload the plugin to the `/wp-content/plugins/` folder.
+2. Then activate the plugin in the plugin panel.
+3. Go to 'Appearance' > 'Widgets' and drag the Recent Posts Widget Plus to the required position in the sidebar.
+5. Type a title for the widget (you can leave this blank)
+6. Type the number of recent posts to show in the widget
+7. Specify the exerpt length by number of characters
+8. Choose a specific category or select `None (all categories)` to show all categories
+9. Choose the post tags you would like to use to filter the list (optional)
+6. Click save.
+
+== Frequently Asked Questions ==
+
+= What is the difference with the default Recent Post widget =
+
+All WordPress installs come with a default widget for showing recent posts. This default widget does neither give you any customization options nor allows you to display an excerpt from the post.
+
+
+== Changelog ==
+
+= 1.0 =
+First release
+
+== Upgrade Notice ==
+
+= 1.0 =
+First release
---
+++ recent-posts-widget-plus.php
@@ -0,0 +1,150 @@
+<?php
+/*
+ Plugin Name: Recent Posts Widget Plus
+ Plugin URI: http://vanderwijk.com
+ Description: A plugin that adds a widget that lists your most recent posts with excerpts. The number of posts and the character limit of the excerpts are configurable.
+ Version: 1.0
+ Author: Johan van der Wijk
+ Author URI: http://www.vanderwijk.com
+ License: GPL2
+
+ Release notes: Version 1.0 Initial release.
+ 
+ Copyright 2012 Johan van der Wijk (email: info@vanderwijk.com)
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License, version 2, as 
+ published by the Free Software Foundation.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+// Custom rpwp_excerpt funtion that allows to limit the number of characters
+function rpwp_excerpt($count){
+	$permalink = get_permalink($post->ID);
+	$excerpt = get_the_content();
+	$excerpt = strip_tags($excerpt);
+	$excerpt = substr($excerpt, 0, $count);
+	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+	$excerpt = $excerpt.'<a href="'.$permalink.'">...</a>';
+	return $excerpt;
+}	
+
+class RecentPostsWidgetPlus extends WP_Widget {
+
+	function RecentPostsWidgetPlus() {
+			$widget_ops = array('classname' => 'recent-posts-plus', 'description' => __( 'Your most recent posts, with optional excerpts') );
+			$this->WP_Widget('RecentPostsWidgetPlus', __('Recent Posts Widget Plus'), $widget_ops);
+	}
+	
+	function widget( $args, $instance ) {
+		extract( $args );
+		
+		$title = apply_filters('widget_title', empty( $instance['title'] ) ? __( 'Recent Posts' ) : $instance['title']);
+		
+		echo $before_widget;
+		echo $before_title . $title . $after_title; ?>
+		
+		<dl>
+		<?php 
+		// Get the recent posts
+		$q = 'showposts='.$instance['numposts'];
+		if (!empty($instance['cat'])) $q .= '&cat='.$instance['cat'];
+		if (!empty($instance['tag'])) $q .= '&tag='.$instance['tag'];
+		query_posts($q);
+			  
+		// Run the loop
+		while (have_posts()) : the_post(); ?>
+			<dt>
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			</dt>
+			<dd>
+				<?php echo rpwp_excerpt($instance['characters']); ?>
+			</dd>
+		<?php endwhile; ?>
+		</dl>
+
+		<?php if($instance['linkurl'] !=""){?>
+			<a href="<?php echo $instance['linkurl']; ?>" class="morelink"><?php echo $instance['linktext']; ?></a>
+		<?php } ?>
+
+		<?php
+		echo $after_widget;
+		wp_reset_query();
+	}	
+	
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['numposts'] = $new_instance['numposts'];
+		$instance['characters'] = $new_instance['characters'];
+		$instance['cat'] = $new_instance['cat'];
+		$instance['tag'] = $new_instance['tag'];
+		$instance['linktext'] = $new_instance['linktext'];
+		$instance['linkurl'] = $new_instance['linkurl'];
+		return $instance;
+	}
+
+	function form( $instance ) {
+		// Widget defaults
+		$instance = wp_parse_args( (array) $instance, array( 
+			'title' => 'Recent Posts',
+			'numposts' => 5,
+			'characters' => 100,
+			'cat' => 0,
+			'tag' => '',
+			'linktext' => '',
+			'linkurl' => '')); ?>  
+
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('numposts'); ?>"><?php _e('Number of posts to show:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('numposts'); ?>" name="<?php echo $this->get_field_name('numposts'); ?>" type="text" value="<?php echo $instance['numposts']; ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('characters'); ?>"><?php _e('Excerpt length in number of characters:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('characters'); ?>" name="<?php echo $this->get_field_name('characters'); ?>" type="text" value="<?php echo $instance['characters']; ?>" />
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('cat'); ?>"><?php _e('Limit to category: '); ?>
+			<?php wp_dropdown_categories(array('name' => $this->get_field_name('cat'), 'show_option_all' => __('None (all categories)'), 'hide_empty'=>0, 'hierarchical'=>1, 'selected'=>$instance['cat'])); ?></label>
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('tag'); ?>"><?php _e('Limit to tags:'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('tag'); ?>" name="<?php echo $this->get_field_name('tag'); ?>" type="text" value="<?php echo $instance['tag']; ?>" />
+			<br /><small><?php _e('Enter post tags separated by commas (\'cat,dog\')'); ?></small>
+		</p>
+
+		<p>
+			<label for="<?php echo $this->get_field_id('linktext'); ?>"><?php _e('Linktext:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('linktext'); ?>" name="<?php echo $this->get_field_name('linktext'); ?>" type="text" value="<?php echo $instance['linktext']; ?>" />
+		</p>
+		
+		<p>
+			<label for="<?php echo $this->get_field_id('linkurl'); ?>"><?php _e('URL:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('linkurl'); ?>" name="<?php echo $this->get_field_name('linkurl'); ?>" type="text" value="<?php echo $instance['linkurl']; ?>" />
+		</p>
+		
+		<?php
+	}
+}
+
+function recent_posts_widget_plus_init() {
+	register_widget('RecentPostsWidgetPlus');
+}
+
+add_action('widgets_init', 'recent_posts_widget_plus_init'); ?>
